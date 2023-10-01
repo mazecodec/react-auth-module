4ea6785ad1f96ff3b6267ee08e424a372e3edf68
Mazecodec <mazecode.tec@gmail.com>
2023-10-01 15:55:20 +0200

    Clean project files unused

--- query/src/app/reducers.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from '@reduxjs/toolkit';
-import authReducer from '../features/auth/slices/authSlice';
-
-const rootReducers = combineReducers({
-  auth: authReducer
-})
-
-export default rootReducers
